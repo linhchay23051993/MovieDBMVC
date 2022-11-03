b0fd25b1affe81efd9c581e5033477eb4fc5a192
LinhChay <nguyenvanlinh23051993@gmail.com>
2022-11-03 20:38:26 +0700

    Start

--- app/src/main/java/com/example/movie/fragment/MovieDetailFragment.java
+++ app/src/main/java/com/example/movie/fragment/MovieDetailFragment.java
@@ -1,4 +1,21 @@
 package com.example.movie.fragment;
 
-public class MovieDetailFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.movie.R;
+
+public class MovieDetailFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.movie_detail_fragment_layout, container, false);
+    }
 }
