a14194a4ef3b92233d4a7af6a6312e758e8f197f
LinhChay <nguyenvanlinh23051993@gmail.com>
2022-11-05 20:06:00 +0700

    Start

--- app/src/main/java/com/example/movie/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/movie/adapter/MovieAdapter.java
@@ -1,4 +1,46 @@
 package com.example.movie.adapter;
 
-public class MovieAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.movie.R;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return new MovieViewHolder(inflater.inflate(R.layout.item_movie_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout movieItem;
+        public ImageView favourite;
+        public TextView title;
+        public TextView rate;
+        public TextView overview;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieItem = itemView.findViewById(R.id.movie_parent_layout);
+        }
+    }
 }
